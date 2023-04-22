ad170cde37be89345c06901f798519319d0cbfc2
royninja <sayan98.roy@gmail.com>
2023-04-22 21:19:03 +0530

    removing unrelated import and cleaning main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
-import LeetCode.String.Easy.Solution1768;
-
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution1768 sol = new Solution1768();
-        System.out.println(sol.mergeAlternately("abc","pqr"));
-        System.out.println(sol.mergeAlternately("ab","pqrs"));
-        System.out.println(sol.mergeAlternately("abcd","pq"));
-        System.out.println(sol.mergeAlternately("","pq"));
-        System.out.println(sol.mergeAlternately("abcd",""));
+
 
 
     }
