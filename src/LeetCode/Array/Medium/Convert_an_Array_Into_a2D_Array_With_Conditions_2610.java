5e14b9a31c927f35515a83900539a0e8b4788310
royninja <sayan98.roy@gmail.com>
2023-04-25 02:42:31 +0530

    adding solution for problem 2610 of leetcode as this was solved also adding a note file

---
+++ src/LeetCode/Array/Medium/Convert_an_Array_Into_a2D_Array_With_Conditions_2610.java
@@ -0,0 +1,37 @@
+package LeetCode.Array.Medium;
+
+import java.util.*;
+/*
+https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/
+https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/solutions/3369344/java-simple-solution-using-hashmap/?orderBy=most_votes&languageTags=java
+needed to check hasmhmap, treeset, hashset, set
+ */
+public class Convert_an_Array_Into_a2D_Array_With_Conditions_2610 {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i],hashMap.getOrDefault(nums[i],0)+1);
+            hashSet.add(nums[i]);
+        }
+        List<List<Integer>> finalList = new ArrayList<>();
+
+        while (hashMap.size() != 0){
+            List<Integer> list = new ArrayList<>();
+
+            for (int item: hashSet) {
+                if(hashMap.containsKey(item)){
+                    int count = hashMap.get(item);
+                    list.add(item);
+                    if(count == 1){
+                        hashMap.remove(item);
+                    }else{
+                        hashMap.put(item, count - 1);
+                    }
+                }
+            }
+            finalList.add(list);
+        }
+        return finalList;
+    }
+}
