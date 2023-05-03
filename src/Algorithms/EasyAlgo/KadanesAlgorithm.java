bd0430d4f787f4eb32fc84a93ca7739f5464b360
royninja <sayan98.roy@gmail.com>
2023-05-04 00:15:36 +0530

    added kadens algorithm with range

--- src/Algorithms/EasyAlgo/KadanesAlgorithm.java
+++ src/Algorithms/EasyAlgo/KadanesAlgorithm.java
@@ -16,4 +16,28 @@ public class KadanesAlgorithm {
         }
         return max_so_far;
     }
+
+    public void KadaensWithRange(int arr[]){
+        int max_ending_here = 0;
+        int max_so_far = Integer.MIN_VALUE;
+        int start = 0;
+        int end = 0;
+        int s = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            max_ending_here += arr[i];
+            if(max_so_far < max_ending_here){
+                max_so_far = max_ending_here;
+                start = s;
+                end = i;
+            }
+
+            if(max_ending_here < 0) {
+                max_ending_here = 0;
+                s = i + 1;
+            }
+        }
+        System.out.println("starting index: "+start);
+        System.out.println("ending index: "+end);
+    }
 }
